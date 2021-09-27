48af1e03360e1cf0d603634b4728861afac3a532
GAURAV200-HUB <kadamgaurv177@gmail.com>
2021-09-27 18:12:07 +0530

    done

--- Leaflet Demo/app.js
+++ Leaflet Demo/app.js
@@ -59,28 +59,32 @@ var leafletIcon = L.icon ({
 // }).addTo(map);
 
 // To add a marker on Map
-var marker = L.marker([19.0759899, 72.8773928], {
-    icon: leafletIcon
-}).addTo(map);
+// var marker = L.marker([19.0759899, 72.8773928], {
+//     icon: leafletIcon
+// }).addTo(map);
 
-// To add a cicle in Map
-var circle = L.circle([19.1348994, 72.8488199], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: '0.5',
-    radius: 1000
-}).addTo(map)
+// // To add a cicle in Map
+// var circle = L.circle([19.1348994, 72.8488199], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: '0.5',
+//     radius: 1000
+// }).addTo(map)
 
-// To add a polygon on Map
-var polygon = L.polygon([
-    [18.921909656029083, 72.83458852767944],
-    [18.922757100812152, 72.83390724658966],
-    [18.921544289288004, 72.83329033851624]
-]).addTo(map)
+// // To add a polygon on Map
+// var polygon = L.polygon([
+//     [18.921909656029083, 72.83458852767944],
+//     [18.922757100812152, 72.83390724658966],
+//     [18.921544289288004, 72.83329033851624]
+// ]).addTo(map)
 
-// To add a Popup message on Map
-marker.bindPopup("<h1>MUMBAI</h1><em>MUMBAI the city of dreams</em>").openPopup();
-circle.bindPopup("<h1>JOGESHWARI</h1><em>My Hometown</em>")
-polygon.bindPopup("<h1>CHURCHGATE</h1><em>Gateway of India</em>")
+// // To add a Popup message on Map
+// marker.bindPopup("<h1>MUMBAI</h1><em>MUMBAI the city of dreams</em>").openPopup();
+// circle.bindPopup("<h1>JOGESHWARI</h1><em>My Hometown</em>")
+// polygon.bindPopup("<h1>CHURCHGATE</h1><em>Gateway of India</em>")
 
-const rectangle = L.rectangle();
+// const rectangle = L.rectangle();
+
+var videoUrl = 'https://www.mapbox.com/bites/00188/patricia_nasa.webm',
+    videoBounds = [[ 32, -130], [ 13, -100]];
+L.videoOverlay(videoUrl, videoBounds ).addTo(map);
