c926357218c8d1ce514432423355a4f182b2234f
GAURAV200-HUB <kadamgaurv177@gmail.com>
2021-09-27 10:49:27 +0530

    .

--- Leaflet Demo/app.js
+++ Leaflet Demo/app.js
@@ -1,7 +1,11 @@
+// To create a basic map in Leaflet Js
 var map = L.map('map').setView([21.125681, 82.79499799999996], 5);
 
 L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mQ0CCTAwmvsVXDOxASms', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(map);
 
-var marker = L.marker([19.0759899, 72.8773928]).addTo(map)
+// To add a marker in Map
+var marker = L.marker([19.0759899, 72.8773928]).addTo(map);
+
+// To add a cicle in Map
