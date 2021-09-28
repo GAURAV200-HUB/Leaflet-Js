64711764796bae602f973b03784b9cd021397286
GAURAV200-HUB <kadamgaurv177@gmail.com>
2021-09-28 17:42:37 +0530

    map added in vue project

---
+++ leaflet-vue/src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <l-map style="height: 400px; width: 800px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2F1cmF2MjAwLWh1YiIsImEiOiJja3UyampkOWkycDZ0Mndtcm5mdHczbncyIn0.UEIXrKw3dRpMn4FUreJDyw",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159],
+    };
+  },
+};
+</script>
+
+<style></style>
